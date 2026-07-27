d5e6f60a3440bf24dcb72c8710bf6ee94dd9c6b5
Lovemore Nyamutsamba <seigendc@gmail.com>
2026-07-27 23:01:12 +0200

    chore(config): connect POS to named Firebase database

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -44,6 +44,7 @@ export const app =
 export const auth = getAuth(app);
 
 export const googleProvider = new GoogleAuthProvider();
+
 googleProvider.setCustomParameters({
   prompt: "select_account",
 });
